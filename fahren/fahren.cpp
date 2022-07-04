c5f6ae67984d211b0b91e60f82853e2df5b1438a
Daniil Rodionov <Rodionovpsy@gmail.com>
2022-07-04 18:43:49 +0300

    first commit

---
+++ fahren/fahren.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int ftemp;
+    cout << "Введите температуру по Фаренгейту:  ";
+    cin >> ftemp;
+    int ctemp = (ftemp -  32) * 5 / 9;
+    cout << "Температура по цельсию равна: " << ctemp << '\n';
+    
+    return 0;
+}
---
+++ intvars/intvars.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int var1;
+    int var2;
+    
+    var1 = 20;
+    var2 = var1 + 10;
+
+    cout << "var1 + 10 равно: ";
+    cout << var2 << endl;
+    // Здесь мы возвращаем значение 0, показывая что программа выполнена успешно
+    return 0;
+}
